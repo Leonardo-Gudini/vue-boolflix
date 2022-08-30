7f2c65f3cb5bb369e1be08291a4e7575152b3cd9
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-08-30 16:55:33 +0200

    inizializzato progetto

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    
+  </header>
+</template>
+
+<script>
+export default {
+    name: "MyHeader"
+}
+</script>
+
+<style lang="scss">
+    header{
+        width: 100%;
+        height: 100px;
+        background-color: #222;
+    }
+</style>
