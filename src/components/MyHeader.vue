6d4348bee4bfd6d36f868acd912147e2d1a0b26e
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-08-30 17:13:00 +0200

    add style

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,6 +1,13 @@
 <template>
   <header>
+    <div class="logo">Boolflix</div>
     
+    <div class="search">
+        <form action="get">
+            <input type="search" name="" id="search">
+            <button type="submit">Cerca</button>
+        </form>
+    </div>
   </header>
 </template>
 
@@ -11,9 +18,39 @@ export default {
 </script>
 
 <style lang="scss">
+    @import "../style/generalStyle.scss";
     header{
         width: 100%;
         height: 100px;
-        background-color: #222;
+        background-color: $bg-color-second;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 40px;
+        .logo{
+            font-weight: bold;
+            font-size: 50px;
+            color: $primary-color;
+            text-transform: uppercase;
+        }
+        #search{
+            height: 50px;
+            width: 200px;
+            background-color: $bg-color-first;
+            border: none;
+            border-radius: 10px;
+            margin-right: 10px;
+            color: white;
+            font-size: 20px;
+            padding: 10px;
+        }
+        button{
+            height: 50px;
+            width: 60px;
+            color: white;
+            background-color: $primary-color;
+            border: none;
+            border-radius: 10px;
+        }
     }
 </style>
