523de79d4ea2b3dbc31f8db48d8fc9368193482d
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-09-01 11:41:54 +0200

    add star vote, add style

--- src/App.vue
+++ src/App.vue
@@ -24,11 +24,6 @@ export default {
   },
   methods: {
     searchFilmSeries(searchText){
-      // axios.get("https://api.themoviedb.org/3/search/movie?api_key=62496f666966773e2ed0aa3c4d10d49c&query=" + searchText + "&language=it-IT")
-      //   .then( res => {
-      //       this.filmSeriesFound = res.data.results;
-      //       console.log(this.filmSeriesFound)
-      //   })
       axios.all([
         axios.get("https://api.themoviedb.org/3/search/movie?api_key=62496f666966773e2ed0aa3c4d10d49c&query=" + searchText + "&language=it-IT"),
         axios.get("https://api.themoviedb.org/3/search/tv?api_key=62496f666966773e2ed0aa3c4d10d49c&query=" + searchText + "&language=it-IT")
--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,32 +1,42 @@
 <template>
 <main>
-    <div>
+    <section>
         <h2>Film</h2>
         <ul>
             
             <li v-for="(film, index) in filmSeriesFound" :key="index">
                 <img :src="'https://image.tmdb.org/t/p/w342/' + film.poster_path" alt="cover"/>
-                <div><span>Titolo: </span>{{film.title}}</div>
-                <div><span>Titolo originale: </span>{{film.original_title}}</div>
-                <country-flag :country='film.original_language' size='medium'/>
-                <div><span>Voto: </span>{{film.vote_average}}</div>
+                <div class="card-text">
+                    <div><span>Titolo: </span>{{film.title}}</div>
+                    <div><span>Titolo originale: </span>{{film.original_title}}</div>
+                    <country-flag :country='film.original_language' size='medium'/>
+                    <div>
+                        <span>Voto: </span>
+                        <i v-for="n in 5" class="fa-star" :class="(n>getVote(film.vote_average))?'fa-regular':'fa-solid'" :key="n"></i>
+                    </div>
+                </div>
             </li>
         </ul>
-    </div>
+    </section>
 
 
-    <div>
+    <section>
         <h2>Serie TV</h2>
         <ul>
             <li v-for="(tvSeries, index) in tvSeriesFound" :key="index">
                 <img :src="'https://image.tmdb.org/t/p/w342/' + tvSeries.poster_path" alt="cover"/>
-                <div><span>Titolo: </span>{{tvSeries.name}}</div>
-                <div><span>Titolo originale: </span>{{tvSeries.original_name}}</div>
-                <country-flag :country='tvSeries.original_language' size='medium'/>
-                <div><span>Voto: </span>{{tvSeries.vote_average}}</div>
+                <div class="card-text">
+                    <div><span>Titolo: </span>{{tvSeries.name}}</div>
+                    <div><span>Titolo originale: </span>{{tvSeries.original_name}}</div>
+                    <country-flag :country='tvSeries.original_language' size='medium'/>
+                    <div>
+                        <span>Voto: </span>
+                        <i v-for="n in 5" class="fa-star" :class="(n>getVote(tvSeries.vote_average))?'fa-regular':'fa-solid'" :key="n"></i>
+                    </div>
+                </div>
             </li>
         </ul>
-    </div>
+    </section>
 </main>
 </template>
 
@@ -41,25 +51,59 @@ export default {
     },
     components:{
         CountryFlag
+    },
+    methods:{
+        getVote(averageVote){
+            return Math.ceil(averageVote / 10 * 5);
+        }
     }
 
 }
 </script>
 
 <style lang="scss">
+    @import '~@fortawesome/fontawesome-free/css/all.css';
     @import "../style/generalStyle.scss";
 
     main{
         color: white;
         padding: 60px;
-        ul{
+        section{
+            margin: 40px 0;
+            h2{
+                font-size: 30px;
+                margin-bottom: 20px;
+                color: $primary-color;
+                border-bottom: 4px solid $bg-color-second;
+            }
+            ul{
             list-style-type: none;
             display: flex;
-            li{
-                margin: 20px;
-                padding: 40px;
-                background-color: $bg-color-second;
+            flex-wrap: wrap;
+                li{
+                    width: calc(100% / 5);
+                    height: 500px;
+                    background-color: $bg-color-second;
+                    .card-text{
+                        display: none;
+                        div{
+                            margin: 10px 0;
+                        }
+                    }
+                    img{
+                        width: 100%;
+                        height: 100%;
+                    }
+                }
+                li:hover img{
+                    display: none;
+                }
+                li:hover .card-text{
+                    display: block;
+                    padding: 30px;
+                }
             }
         }
+        
     }
 </style>
